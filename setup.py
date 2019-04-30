a526df0a6aea4f1668abf55a1ce1b18c0583f85b
Jon Dufresne <jon.dufresne@gmail.com>
2019-04-30 05:07:02 -0700

    Add trove classifier 'Programming Language :: Python :: 3 :: Only'

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
